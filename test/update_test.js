a239f6cff85d66339f9b59bb1857766e45bcb529
camilo17 <cami172013@gmail.com>
2017-05-15 08:24:46 -0500

    added new test using mongodb update operator.

--- test/update_test.js
+++ test/update_test.js
@@ -8,7 +8,7 @@ describe('Updating records', () => {
 
 
     beforeEach((done) => {
-        joe = new User({name: 'Joe'})
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
             .then(() => done());
 
@@ -52,6 +52,15 @@ describe('Updating records', () => {
     it("A model class can find a record with an Id and update", (done) => {
 
         assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done);
+    });
+
+    it('A user can have their postCount incremented by 1', (done) => {
+        User.update({name: 'Joe'}, { $inc: { postCount: 1}})
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.postCount === 1);
+                done();
+            })
     })
 
 
