29aa327795c7761e768d59ecee425bb736a76260
camilo17 <cami172013@gmail.com>
2017-05-15 21:33:18 -0500

    created test to test if subdocument was actually created.

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -18,4 +18,27 @@ describe('Subdocuments', () => {
                     done();
             })
     });
+
+
+    it('Can add subdocuments to an existng record', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            posts: []});
+
+        joe.save()
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                user.posts.push({title: 'New Post'});
+                return user.save();
+            })
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.posts[0].title === 'New Post');
+                done();
+
+            });
+
+
+
+    })
 });
