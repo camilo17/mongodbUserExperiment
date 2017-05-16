574405704983a3f4f708de14b081a7b97c58aacd
camilo17 <cami172013@gmail.com>
2017-05-16 09:02:01 -0500

    Created comment and blogpost Schemas and made refs for them.

--- src/user.js
+++ src/user.js
@@ -12,7 +12,8 @@ const UserSchema = new Schema({
         },
         required: [true, 'Name is required']
     },
-    post: [PostSchema]
+    post: [PostSchema],
+    likes: Number
 });
 
 UserSchema.virtual('postCount').get(function() {
